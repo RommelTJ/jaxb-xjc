b60e160b1db640b16d2612e5f017c5ce14e3a736
Rommel Rico <rommeltj@gmail.com>
2016-03-20 16:59:41 -0700

    Following maven conventions...

---
+++ src/main/java/com/rommelrico/jaxb/JAXBDemo.java
@@ -0,0 +1,13 @@
+package com.rommelrico.jaxb;
+
+import javax.xml.bind.JAXBContext;
+import com.rommelrico.patient.Patient;
+
+
+/**
+ * Created by rommelrico on 3/20/16.
+ */
+
+public class JAXBDemo {
+
+}
