bb71f202e6f6b2de5975c4ab3772216e605beb71
Rommel Rico <rommeltj@gmail.com>
2016-03-20 17:08:05 -0700

    Marshaling and Unmarshalling using JAXBContext

--- src/main/java/com/rommelrico/jaxb/JAXBDemo.java
+++ src/main/java/com/rommelrico/jaxb/JAXBDemo.java
@@ -1,7 +1,14 @@
 package com.rommelrico.jaxb;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
 import com.rommelrico.patient.Patient;
+
+import java.io.StringReader;
+import java.io.StringWriter;
 
 
 /**
@@ -9,5 +16,26 @@ import com.rommelrico.patient.Patient;
  */
 
 public class JAXBDemo {
+
+    public static void main(String[] args) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Patient.class);
+            Marshaller marshaller = context.createMarshaller();
+
+            Patient patient = new Patient();
+            patient.setId(12);
+            patient.setName("Rommel");
+
+            StringWriter writer = new StringWriter();
+            marshaller.marshal(patient, writer);
+            System.out.println(writer.toString());
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Patient patientResult = (Patient)unmarshaller.unmarshal(new StringReader(writer.toString()));
+            System.out.println(patientResult.getName());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
